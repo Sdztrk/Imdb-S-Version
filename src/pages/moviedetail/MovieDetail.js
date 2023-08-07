b00345b263f01c58c1236badd25f409c603e50cd
sdztrk <msaidozturk1@gmail.com>
2023-08-07 02:55:01 +0200

    added info and img part to detail.js

--- src/pages/moviedetail/MovieDetail.js
+++ src/pages/moviedetail/MovieDetail.js
@@ -29,7 +29,26 @@ const MovieDetail = () => {
     }
 
   return (
-    <div>MovieDetail</div>
+    <div className='detailContainer mt-5'>
+      <img className='detailImageBackDrop' src= {`https://image.tmdb.org/t/p/w500/${currentMovieDetail && currentMovieDetail.backdrop_path}`}/>
+      <img className='detailImagePoster' src= {`https://image.tmdb.org/t/p/w500/${currentMovieDetail && currentMovieDetail.poster_path}`}/>
+      <div className='detailInfo'>
+        <h1> {currentMovieDetail&&currentMovieDetail.original_title} </h1>
+        <p> {currentMovieDetail&&currentMovieDetail.tagline} </p>
+        <p> {currentMovieDetail&&currentMovieDetail.runtime} mins </p>
+        <p> Release Date: {currentMovieDetail&&currentMovieDetail.release_date}</p>
+        {currentMovieDetail&&currentMovieDetail.genres.map((genre,index)=> {
+          return(
+            <span 
+            key={index}
+            className='genre mx-1'
+            >
+              {genre.name}
+              </span>
+          )
+        })}
+      </div>
+    </div>
   )
 
 }
